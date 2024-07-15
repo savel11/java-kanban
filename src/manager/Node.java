94a74cc525bf7b9a9630f7bb71f9fc9bcb425df2
Лосев Савелий <saveliy.losev@gmail.com>
2024-07-15 22:18:53 +0300

    Спринт 6 финальный проект 2

--- src/manager/Node.java
+++ src/manager/Node.java
@@ -5,8 +5,8 @@ public Task data;
     public Node<Task> next;
     public Node<Task> prev;
 
-    public Node(Task task, Node<Task> prev, Node<Task> next) {
-        this.data = task;
+    public Node(Task data, Node<Task> prev, Node<Task> next) {
+        this.data = data;
         this.next = next;
         this.prev = prev;
     }
