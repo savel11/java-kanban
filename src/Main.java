157f4bc596607bd567f9a4161c311301e1cbe212
Лосев Савелий <saveliy.losev@gmail.com>
2024-08-25 20:13:28 +0300

    Спринт09. Добавить API, добавить тесты

--- src/Main.java
+++ src/Main.java
@@ -15,31 +15,31 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-            HttpTaskServer httpTaskServer = new HttpTaskServer(Managers.getDefault());
-            httpTaskServer.start();
-        }
+        HttpTaskServer httpTaskServer = new HttpTaskServer(Managers.getDefault());
+        httpTaskServer.start();
+    }
+}
+
+class HttpTaskServer {
+    HttpServer httpServer;
+    TaskManager taskManager;
+
+    public HttpTaskServer(TaskManager taskManager) throws IOException {
+        this.taskManager = taskManager;
+        httpServer = HttpServer.create(new InetSocketAddress(8080), 0);
+        httpServer.createContext("/tasks", new TasksHttpHandler(taskManager));
+        httpServer.createContext("/subtasks", new SubtasksHttpHandler(taskManager));
+        httpServer.createContext("/epics", new EpicsHttpHandler(taskManager));
+        httpServer.createContext("/history", new HistoryHttpHandler(taskManager));
+        httpServer.createContext("/prioritized", new PrioritizedHttpHandler(taskManager));
     }
 
-    class HttpTaskServer {
-        HttpServer httpServer;
-        TaskManager taskManager;
-
-        public HttpTaskServer(TaskManager taskManager) throws IOException {
-            this.taskManager = taskManager;
-            httpServer = HttpServer.create(new InetSocketAddress(8080), 0);
-            httpServer.createContext("/tasks", new TasksHttpHandler(taskManager));
-            httpServer.createContext("/subtasks", new SubtasksHttpHandler(taskManager));
-            httpServer.createContext("/epics", new EpicsHttpHandler(taskManager));
-            httpServer.createContext("/history", new HistoryHttpHandler(taskManager));
-            httpServer.createContext("/prioritized", new PrioritizedHttpHandler(taskManager));
-        }
-
-        public void start() {
-            httpServer.start();
-        }
-
-        public void stop() {
-            httpServer.stop(1);
-        }
+    public void start() {
+        httpServer.start();
     }
+
+    public void stop() {
+        httpServer.stop(1);
+    }
+}
 
